abf18145f56c58e5419b656dbc61c6d83782b16b
Lucas Aguero <Lucas-Aguero@ajvierci.com.py>
2023-07-05 16:43:46 -0400

    work 6

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 export const AboutPage = () => {
   return (
-    <div
-    className="border-red-500 border-2 justify-center items-center flex w-full h-full"
-    > AboutPage</div>
+    <div className="flex-col border-red-500 border-2 justify-center items-center flex w-full h-full">
+       <h1 className='border-2 border-green-500'>TITLE</h1>
+       <div className='border-2 border-red-200'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dolorem aperiam, sed sapiente eos fugiat quibusdam eius, 
+            ipsa ex obcaecati quis vero? Possimus nesciunt dolor dicta perferendis, fuga assumenda a.
+          </p>
+       </div>
+       
+    </div>
   )
 }
--- src/pages/Skills/index.jsx
+++ src/pages/Skills/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { frontEndTech, backEndTech, otherTech } from '@/data' 
 
 export const SkillPage = () => {
   return (
-    <div
-    className="border-yellow-500 border-2 justify-center items-center flex w-full h-full"
-    >SkillPage</div>
+    <div className="border-yellow-500 border-2 justify-center items-center flex-col gap-2 flex w-full h-full">
+      <h1 className='border-2 border-yellow-300'>TITLE</h1>
+      <section className="border-pink-700 border-2 p-2 h-2/5 flex flex-col gap-4">
+        <div className='border-2 border-blue-700 flex gap-2 flex-row'>
+          <div id='frontend' className='bg-green-500'>
+            asfapofhpoahf ahfòahpfh paihfihaf
+
+          </div>
+          <div id='backend' className='bg-orange-200'>
+            asfapofhpoahf ahfòahpfh paihfihaf
+          </div>
+        </div >
+          <div id='other' className='border-2 border-red-950'> 
+            asfapofhpoahf ahfòahpfh paihfihaf
+          </div>
+      </section>
+    </div>
   )
 }
