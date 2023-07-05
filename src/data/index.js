84d2340c4ae42dc4485ac6df2e411b97f11d226a
Lucas Aguero <Lucas-Aguero@ajvierci.com.py>
2023-07-05 07:50:11 -0400

    work 5

---
+++ src/data/index.js
@@ -0,0 +1,54 @@
+export const frontEndTech = [
+    {
+        id: 1,
+        skillName: "ReactJS"   
+    },
+    {
+        id: 2,
+        skillName: "Tailwindcss"
+    },
+    {
+        id: 3,
+        skillName: "TypeScript"
+    },
+    {
+        id: 4,
+        skillName: "JavaScript"
+    },
+    {
+        id: 5,
+        skillName: "HTML"
+    },
+    {
+        id: 6,
+        skillName: "CSS"
+    },
+]
+
+export const backEndTech = [
+    {
+        id: 1,
+        skillName: "NodeJS"
+    },
+    {
+        id: 2,
+        skillName: "MongoDB"
+    },
+    {
+        id: 3,
+        skillName: "ExpressJS"
+    },
+]
+
+export const otherTech = [
+    {
+        id: 1,
+        skillName: "Vite"
+    },
+    {
+        id: 2,
+        skillName: "Git"
+    },
+   
+
+]
