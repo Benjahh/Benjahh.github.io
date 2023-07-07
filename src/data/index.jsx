3c39fb68b09837d342477b3c4916b8a45364dbb9
Lucas Aguero <Lucas-Aguero@ajvierci.com.py>
2023-07-07 08:02:30 -0400

    work 8

---
+++ src/data/index.jsx
@@ -0,0 +1,102 @@
+import { 
+    SiExpress, 
+    SiNodedotjs, 
+    SiTailwindcss,
+    SiJavascript,
+    SiTypescript,
+    SiReact,
+    SiVite,
+    SiHtml5,
+    SiCss3,
+    SiMongodb,
+    SiGit,
+
+} from "react-icons/si"
+
+export const frontEndTech = [
+    {
+        id: 1,
+        skillName: "ReactJS",
+        Icon:  <SiReact/> 
+    },
+    {
+        id: 2,
+        skillName: "Tailwindcss",
+        Icon: <SiTailwindcss/>
+    },
+    {
+        id: 3,
+        skillName: "TypeScript",
+        Icon: <SiTypescript/>
+    },
+    {
+        id: 4,
+        skillName: "JavaScript",
+        Icon: <SiJavascript/>
+    },
+    {
+        id: 5,
+        skillName: "HTML",
+        Icon: <SiHtml5/>
+    },
+    {
+        id: 6,
+        skillName: "CSS",
+        Icon: <SiCss3/>  
+    },
+]
+
+export const backEndTech = [
+    {
+        id: 1,
+        skillName: "NodeJS",
+        Icon: <SiNodedotjs/>  
+    },
+    {
+        id: 2,
+        skillName: "MongoDB",
+        Icon: <SiMongodb/>  
+    },
+    {
+        id: 3,
+        skillName: "ExpressJS",
+        Icon: <SiExpress/>
+    },
+]
+
+export const otherTech = [
+    {
+        id: 1,
+        skillName: "Vite",
+        Icon: <SiVite/>
+    },
+    {
+        id: 2,
+        skillName: "Git",
+        Icon: <SiGit/>
+    },
+]
+
+export const projects = [
+    {
+        id: 1,
+        projectName: "SocialApp",
+        projectLink : "",
+        projectSkill: [
+            {
+                id: 1,
+
+            }
+    ]
+    },
+    {
+        id: 2, 
+        projectName: "FitnessApp",
+        projectLink: "",
+        projectSkill: [
+            {
+                id: 1
+            }
+        ]
+    }
+]
--- src/pages/Skills/index.jsx
+++ src/pages/Skills/index.jsx
@@ -5,20 +5,36 @@ export const SkillPage = () => {
   return (
     <div className="border-yellow-500 border-2 justify-center items-center flex-col gap-2 flex w-full h-full">
       <h1 className='border-2 border-yellow-300'>TITLE</h1>
-      <section className="border-pink-700 border-2 p-2 h-2/5 flex flex-col gap-4">
-        <div className='border-2 border-blue-700 flex gap-2 flex-row'>
-          <div id='frontend' className='bg-green-500'>
-            asfapofhpoahf ahfòahpfh paihfihaf
+      <div className="border-pink-700 border-2 p-2 h-2/5 flex flex-col gap-4">
+        <section className='border-2 border-blue-700 flex gap-2 flex-row'>
 
+          <div id='frontend' className='bg-green-500'>
+            {frontEndTech.map((frontEndTech)=>(
+              <div key={frontEndTech.id}>
+                {frontEndTech.skillName}
+              </div>
+            ))}
           </div>
+
           <div id='backend' className='bg-orange-200'>
-            asfapofhpoahf ahfòahpfh paihfihaf
+            {backEndTech.map((backEndTech)=>(
+              <div key={backEndTech}>
+                {backEndTech.skillName}
+              </div>
+            ))}
           </div>
-        </div >
+
+        </section >
+
           <div id='other' className='border-2 border-red-950'> 
-            asfapofhpoahf ahfòahpfh paihfihaf
+           {otherTech.map((otherTech)=>(
+            <div key={otherTech.id}>
+              {otherTech.skillName}
+            </div>
+           ))}
           </div>
-      </section>
+
+      </div>
     </div>
   )
 }
