b06e3a289261d468bf8376652a1233fb90a8638f
Benjahh <lucasbenja09@hotmail.com>
2024-01-22 22:12:12 -0300

    feat: animations, gif assets and additional styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Home } from '@/pages/home';
 
 function App() {
   return (
-    <main className="relative bg-noise flex flex-col py-32 items-center justify-center  text-white h-auto w-auto">
+    <main className="relative bg-noise  flex flex-col items-center justify-center  text-white h-auto w-auto">
       <Home />
     </main>
   );
--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -2,8 +2,9 @@ import { techStack } from '@/lib/data';
 
 export const TechStack = ({ techName, TechIcon }) => {
   return (
-    <div className="row-span-1 flex items-center justify-center rounded-md  col-span-1">
-      <div className="bg-black  p-4  rounded-lg bg-opacity-35">{TechIcon}</div>
+    <div className="row-span-1 w-[100px] p-3 bg-opacity-35 bg-black items-center flex flex-col justify-center  rounded-lg col-span-1">
+      <div className="  ">{TechIcon}</div>
+      <div>{techName}</div>
     </div>
   );
 };
