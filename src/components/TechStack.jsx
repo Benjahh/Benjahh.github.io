eb45df2bed38d2334bcb922b7fe9acff1db37162
Benjahh <lucasbenja09@hotmail.com>
2024-01-24 22:17:06 -0300

    feat: more styling overall, textual corrections and verbose

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -2,7 +2,7 @@ import { techStack } from '@/lib/data';
 
 export const TechStack = ({ techName, TechIcon }) => {
   return (
-    <div className="row-span-1 w-[100px] p-3 bg-opacity-35 bg-black items-center flex flex-col justify-center  rounded-lg col-span-1">
+    <div className="row-span-1 w-[90px] p-3 bg-opacity-35  text-sm bg-black items-center flex flex-col justify-center  rounded-lg col-span-1">
       <div className="  ">{TechIcon}</div>
       <div>{techName}</div>
     </div>
